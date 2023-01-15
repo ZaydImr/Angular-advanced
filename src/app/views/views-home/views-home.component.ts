52f79b4b51dccd4dfa7e7ace7bfbd58ecdce0270
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-01-15 07:47:53 +0100

    Adapt to server

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -14,8 +14,8 @@ export class ViewsHomeComponent implements OnInit {
   ];
 
   items = [
-    { image: '/assets/images/couch.jpg', title: 'Couch', description:'This is a fantastic couch grey to sit on' },
-    { image: '/assets/images/couch_blue.webp', title: 'Couch Blue', description:'This is a fantastic couch blue to sit on' }
+    { image: 'assets/images/couch.jpg', title: 'Couch', description:'This is a fantastic couch grey to sit on' },
+    { image: 'assets/images/couch_blue.webp', title: 'Couch Blue', description:'This is a fantastic couch blue to sit on' }
   ];  
 
   constructor() { }
